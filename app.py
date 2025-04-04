52f482a3c82e6b3d42a8c3c5fecea21da9b46da8
ao <rajpowell153@gmail.com>
2025-04-04 19:40:59 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        name = request.form['name']
+        message = request.form['message']
+        return render_template('result.html', name=name, message=message)
+    return render_template('form.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        name = request.form['name']
+        message = request.form['message']
+        return render_template('result.html', name=name, message=message)
+    return render_template('form.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False)
+
